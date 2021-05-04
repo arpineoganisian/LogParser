fcb322451827489f90ccc07466d59117ad40d81e
Herminia Woodwright <hwoodwri@si-j2.kzn.21-school.ru>
2021-05-04 20:29:27 +0300

    d

---
+++ src/LogParser.java
@@ -0,0 +1,12 @@
+package hw07;
+
+import java.nio.file.Path;
+
+public class LogParser {
+
+//    logDir - директория с логами (логов может быть несколько, все они должны иметь расширение log)
+
+    public LogParser(Path logDir) {
+
+    }
+}
