182010ccb3183a193666494bf1cdb3e987989a98
Herminia Woodwright <hwoodwri@si-i4.kzn.21-school.ru>
2021-05-05 17:48:59 +0300

    validation for args and directory is added

--- src/LogParser.java
+++ src/LogParser.java
@@ -1,12 +1,22 @@
-package hw07;
-
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+//Matcher и Pattern
 
 public class LogParser {
 
-//    logDir - директория с логами (логов может быть несколько, все они должны иметь расширение log)
+    //    logDir - директория с логами (логов может быть несколько, все они должны иметь расширение log)
+
+    Path logDir;
 
     public LogParser(Path logDir) {
-
+        this.logDir = logDir;
     }
+
+//    public void parse() {
+//        Files files = new Files();
+//        List<String> lines =
+//    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-package hw07;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.out.println("1 argument needed");
+        }
+
+        if (!(Files.isDirectory(Paths.get(args[0])))) {
+            System.out.println("There is no such directory: " + Paths.get(args[0]).toString());
+            return;
+        }
+        // Метод get() класса Paths - из переданной строки или URI получает объект типа Path
+        LogParser logParser = new LogParser(Paths.get(args[0]));
+
+        //Путь
+        System.out.println(logParser.logDir.toString());
+
+
+    }
 }
